86007626c06a76559a9657620d05d676ccc37921
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-07-25 02:43:05 +0600

    chada svg done

--- components/Boards/Chadaa.tsx
+++ components/Boards/Chadaa.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function Chadaa() {
+export function ChadaSVG(props: any) {
+
+    let a1 = props.item.a1
+    let a2 = props.item.a2
+
     return (
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 501.23 251.61">
+        <div className='grid gap-2 justify-center'>
+            <svg className='h-28 w-56' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 501.23 251.61">
                 <g id="scales">
                     <line style={{ fill: 'none', stroke: '#f90606', strokeMiterlimit: '10', strokeWidth: '2px' }} x1="250.61" y1="250.61" x2="250.61" y2="1" />
                     <line style={{ fill: 'none', stroke: '#f90606', strokeMiterlimit: '10', strokeWidth: '2px' }} x1="250.61" y1="250.61" x2="230.22" y2="17.5" />
@@ -50,15 +54,82 @@ function Chadaa() {
                         transform="translate(-109.39 -387.05)" />
                 </g>
                 <g id="pointers">
-                    <line style={{ fill: 'none', strokeMiterlimit: '10', strokeWidth: '2px', stroke: '#069a49' }} x1="250.61" y1="28.03" x2="250.61" y2="250.61" />
-                    <line style={{ fill: 'none', strokeMiterlimit: '10', strokeWidth: '2px', stroke: '#069a49' }} x1="126.15" y1="66.08" x2="250.61" y2="250.61" />
+
+                    {/* <line style={{ fill: 'none', strokeMiterlimit: '10', strokeWidth: '2px', stroke: 'gray' }}
+                                    x1="250.61" y1="28.03" x2="250.61" y2="250.61"
+                                    transform="rotate(0 250.61 250.61)"
+                                /> */}
+
+                    <line style={{ fill: 'none', strokeMiterlimit: '10', strokeWidth: '2px', stroke: 'blue' }}
+                        x1="250.61" y1="28.03" x2="250.61" y2="250.61"
+                        transform={`rotate(${-a1 + 90} 250.61 250.61)`}
+                    />
+                    <line style={{ fill: 'none', strokeMiterlimit: '10', strokeWidth: '2px', stroke: 'green' }}
+                        x1="250.61" y1="28.03" x2="250.61" y2="250.61"
+                        transform={`rotate(${90 - a2} 250.61 250.61)`}
+                    />
+
                     <path style={{ fill: 'none', strokeMiterlimit: '10', strokeWidth: '2px', stroke: '#231f20' }}
                         d="M582.59,637.67c0-122.94-99.66-222.59-222.59-222.59S137.41,514.73,137.41,637.67Z"
                         transform="translate(-109.39 -387.05)" />
                 </g>
             </svg>
+            <p className='text-xs text-center'>({props.x}) {props.shows ? (<span className='border-b border-black'>{a2 - a1}°</span>) : (<span>_____</span>)}</p>
+
         </div>
     )
 }
 
-export default Chadaa
+export function ChadaQ(props) {
+    return (
+        <div className='grid justify-center bg-gray-50 rounded-md'>
+            <div className="relative flex justify-center rounded-t-full ring-1 ring-black h-28 w-56 bg-gray-100">
+                <div className='flex gap-[0.5px] h-28 relative justify-center'>
+
+                    <div style={{ transform: 'rotate(-85deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-80deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-75deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-70deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-65deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-60deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-55deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-50deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-45deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-40deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-35deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-30deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-25deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-20deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-15deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-10deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(-5deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+
+                    <div style={{ transform: 'rotate(0deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(5deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(10deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(15deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(20deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(25deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(30deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(35deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(40deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(45deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(50deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(55deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(60deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(65deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(70deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(75deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(80deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    <div style={{ transform: 'rotate(85deg)', transformOrigin: 'bottom' }} className='h-[130px] absolute bottom-0 p-[0.5px] bg-black z-20' />
+                    {/* <div style={{ transform: 'rotate(90deg)', transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-black z-20' /> */}
+                </div>
+                <div className='flex justify-center absolute bottom-0 rounded-t-full h-[120px] w-[240px] bg-white z-40'>
+                    <div style={{ transform: `rotate(${props.low}deg)`, transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-[red] z-20' />
+                    <div style={{ transform: `rotate(${props.high}deg)`, transformOrigin: 'bottom' }} className='h-full absolute bottom-0 p-[0.5px] bg-[red] z-20' />
+                </div>
+            </div>
+            <p className='text-xs text-center py-2'>({props.x}) {props.shows ? (<span className='border-b border-black'>{props.high - props.low}°</span>) : (<span>_____</span>)}</p>
+        </div>
+    )
+}
